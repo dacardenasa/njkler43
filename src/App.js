b7e22ccc0b8ef21c91d327b5a4e80c6b0cffbb7f
dacardenasa <diego.cardenasaleg@gmail.com>
2020-07-07 19:39:20 -0500

    Add handle change method to App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import posts from './posts'
 
-
-// Modifica el componente App para implmentar la funcionalidad requerida
-
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      postsList: posts 
+    }
+  }
+
+  handleChange(event){
+    let word = (event.target.value).toLowerCase();
+    this.setState({
+      postsList: posts.filter((post) => 
+        post.title.toLowerCase().includes(word)
+      )
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input 
+          type="text"
+          onChange={this.handleChange.bind(this)}
+          placeholder="Ingresa el término de búsqueda" />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {this.state.postsList.map( (post, index) => 
+            <li key={index}>
+              <a href={post.url}><img src={post.image } alt={post.title}/></a>
+              <p>{ post.title }</p>
+            </li>
+          )}
         </ul>
       </div>
     )
